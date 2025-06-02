283ff07466005fd959bda8bc7f2adbdac3fc0558
MaksymDramaretskyi-1 <maksym.glodedata@gmail.com>
2025-06-02 10:58:48 +0200

    js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log('Hello!');
+
+const addTodo = document.getElementById("addTodo");
+console.log(addTodo);
+
+
+addTodo.addEventListener("click", function(event) {
+  event.preventDefault();
+  console.log("Button click?");
+
+  const todoInput = document.getElementById("todoInput");
+  console.log(todoInput);
+  const todoInputValue = todoInput.value;
+  console.log(todoInputValue);
+
+  const todoDisplay = document.getElementById("todoDisplay");
+  console.log(todoDisplay);
+  todoDisplay.textContent = todoInputValue;
+
+});
